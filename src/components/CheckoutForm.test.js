b85b34aefa3d6a2f97224d37e2a5bfcabdf472d1
Uros Ivanovic <urosivanovic@Uross-MBP.lan>
2022-01-14 12:08:15 -0500

    testing done

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,10 +1,45 @@
 import React from "react";
-import MutationObserver from 'mutationobserver-shim';
-import { render } from "@testing-library/react";
+// import MutationObserver from 'mutationobserver-shim';
+import { render, screen } from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("renders without errors", () => {});
 
-test("shows success message on submit with form details", () => {});
+test("renders without errors", () => {
+    render (<CheckoutForm />)
+    const header = screen.queryByText(/checkout form/i);
+    expect(header).toBeInTheDocument();
+    expect(header).toBeTruthy();
+    expect(header).toHaveTextContent(/checkout form/i);
+
+});
+
+test("shows success message on submit with form details", () => {
+    render(<CheckoutForm />)
+
+    const typeFirst = screen.getByLabelText(/first name:/i)
+    const typeLast = screen.getByLabelText(/last name:/i)
+    const typeAddress = screen.getByLabelText(/address:/i)
+    const typeCity = screen.getByLabelText(/city:/i)
+    const typeState = screen.getByLabelText(/state:/i)
+    const typeZip = screen.getByLabelText(/zip:/i)
+    const button = screen.getByRole('button')
+
+    const successMsg = () => screen.getByTestId('successMessage')
+
+    userEvent.type(typeFirst, 'Bugsy')
+    userEvent.type(typeLast, 'Bugs')
+    userEvent.type(typeAddress, '88 Remsen St')
+    userEvent.type(typeCity, 'Brooklyn')
+    userEvent.type(typeState, 'New York')
+    userEvent.type(typeZip, '11201')
+    userEvent.click(button)
+
+    expect(successMsg()).toBeInTheDocument()
+    expect(successMsg().children[4]).toHaveTextContent('Bugsy Bugs')
+    expect(successMsg().children[5]).toHaveTextContent('88 Remsen St')
+    expect(successMsg().children[6]).toHaveTextContent('Brooklyn, New York 11201')
+
+});
